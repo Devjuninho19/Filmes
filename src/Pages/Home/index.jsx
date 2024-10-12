f62b1305d531be75d3e6d7474f7b81975f2d4bd8
devjuninho19 <fareisorrir@gmail.com>
2024-10-12 13:41:59 -0300

    map na api realizado para trazer todos os filmes listados

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import api from '../../service/api'
+import { Link } from 'react-router-dom'
+import "./home.css"
 const index = () => {
+  
+  const [ filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+    async function loadFilme(){
+    const response = await api.get("movie/now_playing", {
+      params:{
+        api_key:"5dd2d4b6b65d07a72aa5f7bb7798ee02",
+        language:"pt-BR",
+        page:1,
+      }
+    })
+setFilmes(response.data.results)
+    }
+    loadFilme();
+  },[])
   return (
-    <div>
-        <h1>Bem vindo a home</h1>
+    <div className='container'>
+       <div className='lista-filmes'>
+{filmes.map((filme)=>{
+  return(
+    <article key={filme.id}>
+      <strong>{filme.title}</strong>
+      <img src={`https://image.tmdb.org/t//p/original/${filme.poster_path}`} alt={filme.title} />
+      <Link to={`/filme/${filme.id}`}>Acessar</Link>
+    </article>
+  )
+})}
+       </div>
     </div>
   )
-}
+} 
 
 export default index
